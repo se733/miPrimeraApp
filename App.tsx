47fcaec390ef80bd5682501efd4ab5b4e7201f94
se733 <chavezsebas737@gmail.com>
2023-01-30 18:13:11 -0300

    BoxObjectModel

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen'
 import { ContadorScreen } from './src/screens/ContadorScreen'
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
+import { StyleSheet } from 'react-native';
 
 
 export const App = () => {
   return (
     // <HolaMundoScreen />
-    <ContadorScreen/>
+    // <ContadorScreen/>
+    <BoxObjectModelScreen />
   )
 }
+const styles = StyleSheet.create({
+    
+});
